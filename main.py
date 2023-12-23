60fc1d9a34dcb5a6e8d4eb217c23d3d249b44820
milena222-a <mlnillarionova@gmail.com>
2023-12-24 00:20:18 +0700

    1.2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from sklearn.preprocessing import LabelBinarizer
-lb = LabelBinarizer()
-one_hot_encoded = lb.fit_transform(data['whoAmI'])
-one_hot_df = pd.DataFrame(one_hot_encoded, columns=lb.classes_)
-data = data.join(one_hot_df)
-data.head()
+import pandas as pd
+
+data = pd.DataFrame({
+    'whoAmI': ['cat', 'dog', 'dog', 'cat', 'rabbit']
+})
