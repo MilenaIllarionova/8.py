27ab8367b8596485b1797c2939a213e86720d383
milena222-a <mlnillarionova@gmail.com>
2023-12-24 00:20:50 +0700

    1.3

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import pandas as pd
 data = pd.DataFrame({
     'whoAmI': ['cat', 'dog', 'dog', 'cat', 'rabbit']
 })
+one_hot = pd.get_dummies(data['whoAmI'])
+data = data.drop('whoAmI', axis=1)
+data = data.join(one_hot)
+print(data)
