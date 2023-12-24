5aa86de35acaf6a5cf0cf9fca2ac796b575b27f4
milena222-a <mlnillarionova@gmail.com>
2023-12-24 13:58:41 +0700

    3

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import random
 import pandas as pd
 
-data = pd.DataFrame({
-    'whoAmI': ['cat', 'dog', 'dog', 'cat', 'rabbit']
-})
+lst = ['robot'] * 10 + ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+# Создаем пустой DataFrame с колонками для каждого уникального значения из столбца 'whoAmI'
 one_hot = pd.get_dummies(data['whoAmI'])
-data = data.drop('whoAmI', axis=1)
-data = data.join(one_hot)
-print(data)
+
+# Объединяем исходный DataFrame с полученным one-hot DataFrame
+data_one_hot = pd.concat([data, one_hot], axis=1)
+
+data_one_hot.head()
+
